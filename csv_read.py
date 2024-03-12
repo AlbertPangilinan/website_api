1698e90232539de490c98252be64a39155c7858c
AlbertPangilinan <rayalbertz@gmail.com>
2024-03-12 18:35:14 -0400

    added id key and text/tags list splitting

--- csv_read.py
+++ csv_read.py
@@ -4,8 +4,14 @@ projects = []
 
 with open('projects.csv', newline='') as file:
     reader = csv.DictReader(file)
+    i = 0
     for row in reader:
+        row["id"] = i
+        row["text"] = row["text"].split('|')
+        row["tags"] = row["tags"].split('|')
         projects.append(row)
+        i += 1
+
 
 def get_all_projects():
     return projects
