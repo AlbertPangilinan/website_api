9c92a4599cd58d8324b9bf3fb19729d3cac74c1f
AlbertPangilinan <rayalbertz@gmail.com>
2024-03-15 18:55:40 -0400

    added flask_cors and CORS config to server

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import csv_read
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, origins=["http://localhost:3000/", "http://localhost:3000"])
 
 @app.route("/")
 def home():
